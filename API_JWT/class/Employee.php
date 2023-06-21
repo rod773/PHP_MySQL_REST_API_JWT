83a1e907dca905b79ea6966b047ad632d8c3847a
rod773 <104315743+rod773@users.noreply.github.com>
2023-06-21 18:12:03 +0200

    employee class

--- API_JWT/class/Employee.php
+++ API_JWT/class/Employee.php
@@ -10,4 +10,36 @@
  */
 class Employee
 {
+    private $empTable = "tbl_employees";
+    private $id;
+    private $name;
+    private $email;
+    private $designation;
+    private $connection;
+
+
+    //database init
+    public function __construct($db){
+        $this->connextion = $db;
+    }
+
+    //insert
+    public function addEmployee(){
+
+    }
+
+    //read
+    public function listEmployee(){
+
+    }
+
+    //update
+    public function updateEmployee(){
+
+    }
+
+    //delete
+    public function deleteEmployee(){
+
+    }
 }
