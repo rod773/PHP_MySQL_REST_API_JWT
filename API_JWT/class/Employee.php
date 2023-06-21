a37a9f1381793512c24fe9c3756f193fe8ae8659
rod773 <104315743+rod773@users.noreply.github.com>
2023-06-21 19:14:36 +0200

    function add employee

--- API_JWT/class/Employee.php
+++ API_JWT/class/Employee.php
@@ -20,12 +20,31 @@ class Employee
 
     //database init
     public function __construct($db){
-        $this->connextion = $db;
+        $this->connection = $db;
     }
 
     //insert
     public function addEmployee(){
+        $sql = "insert into $this->empTable (name, email, designation) "
+            + "values (?,?,?)";
 
+        if($stmt = $this->connection->prepare($sql)){
+
+            $this->name = htmlspecialchars(strip_tags($this->name));
+
+            $this->name = htmlspecialchars(strip_tags($this->email));
+
+            $this->name = htmlspecialchars(strip_tags($this->designation));
+
+            $stmt->bind_param("sss",$this->name,$this->email,$this->designation);
+
+            if($stmt->execute){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
 
     //read
@@ -48,10 +67,10 @@ class Employee
 function json($status,$message,$data=array()){
 
     $data = array(
-        "status"=>$status,
-        "maeage"=>$message,
-        "data"=>$data,
-     )
+        "status" => $status,
+        "maeage" => $message,
+        "data" => $data,
+    );
 
      $json_encoded_data = json_encode($data);
 
