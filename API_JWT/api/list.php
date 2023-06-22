dcf8d9f8a4c1e32950d8cbfde40cadc718f0d4fc
rod773 <104315743+rod773@users.noreply.github.com>
2023-06-22 16:14:52 +0200

    list  all employees php

--- API_JWT/api/list.php
+++ API_JWT/api/list.php
@@ -1,3 +1,31 @@
 <?php
 
+    include_once "../database/Database.php";
+
+    include_once "../class/Employee.php";
+    //ini_set("display_errors",1);
+
+
+    $db = new Database();
+
+    $mysqli = $db->getConection();
+
+    $emp = new Employee($mysqli);
+
+    $result = $emp->listEmployee();
+
+    if($result->num_rows>0){
+
+        $allEmployees = $result->fetch_all(MYSQLI_ASSOC);
+
+        http_response_code(200);
+        json(1,"Enployee data",$allEmployees);
+    }
+    else{
+        http_response_code(404);
+        json(0,"No employee found");
+    }
+
+
+
 ?>
