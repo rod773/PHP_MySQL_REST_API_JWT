21362b8d15544a74cd5be0d56f9d9e986229e68d
rod773 <104315743+rod773@users.noreply.github.com>
2023-06-22 18:46:03 +0200

    update employee php

--- API_JWT/api/update.php
+++ API_JWT/api/update.php
@@ -1,3 +1,55 @@
 <?php
+    include_once "../database/Database.php";
+
+    include_once "../class/Employee.php";
+    //ini_set("display_errors",1);
+
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+
+
+    $db = new Database();
+
+    $mysqli = $db->getConection();
+
+    $emp = new Employee($mysqli);
+
+    $formdata = json_decode(file_get_contents("php://input"));
+
+
+    if(
+        !empty($formdata->name) &&
+        !empty($formdata->email) &&
+        !empty($formdata->designation &&
+        !empty($formdata->id)
+        )
+        ){
+
+        $name = $formdata->name;
+        $email = $formdata->email;
+        $designation = $formdata->designation;
+        $id = $formdata->id;
+
+        if($emp){
+           if($emp->updateEmployee($name,$email,$designation,$id)){
+               http_response_code(200);
+               json(1,"employee updated suscessfully");
+           }
+         else{
+             http_response_code(500);
+             json(0,"failed to update employee");
+           }
+        }
+    }
+    else{
+        http_response_code(404);
+        json(0,"input values required");
+    }
+
+    }
+    else{
+        http_response_code(500);
+        json(0,"Invalid Request Method");
+    }
+
 
 ?>
